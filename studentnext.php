6fd9a52d3fda1cc2db5452e10af3e57a57db6e71
mufyraees <56551327+mufyraees@users.noreply.github.com>
2019-10-22 03:55:33 -0500

    Add files via upload

---
+++ studentnext.php
@@ -0,0 +1,15 @@
+<html>
+
+<form action="studenttest.php" method="post">
+
+
+<select id="level" name="levels">
+        <option value="1">level 1</option>
+        <option value="2">level 2</option>
+        <option value="3">level 3</option>
+	  </select>
+	
+<input type="submit" name="next" value="NEXT"/>
+</form>
+
+</html>
