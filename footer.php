1e0096334477c968e3b1d3dd0d7ddc51e2a6b8c0
mufyraees <56551327+mufyraees@users.noreply.github.com>
2019-11-12 20:59:48 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,116 @@
+ 
+          <div class="col-md-8 mt-lg-5 text-center">
+
+            
+        </div>
+      </div>
+
+    
+    </div>  
+
+    
+<div class="site-section cta-big-image" id="about-section">
+  <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center" data-aos="fade">
+            <h2 class="section-title mb-3">About O F O R I</h2>
+          </div>
+        </div>
+    <div class="row">
+          <div class="col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
+            <figure class="circle-bg">
+            <img src="images/hero_1.jpg" alt="Image" class="img-fluid">
+            </figure>
+          </div>
+      <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="100">
+        <div class="mb-4">
+          <h3 class="h3 mb-4 text-black">MENTAL HEALTH DEVELOPMENT</h3>
+			  <h3 class="h3 mb-4 text-black">ONLINE ASSESMENT TEST</h3>
+        </div>
+             
+          </div>
+        </div>
+      </div>  
+    </div>
+
+    
+    <section class="site-section" id="about-section"></section>
+<section class="site-section" id="blog-section"></section>
+
+   
+
+
+    <section class="site-section bg-light" id="contact-section" data-aos="fade">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <h2 class="section-title mb-3">Contact Us</h2>
+          </div>
+        </div>
+        <div class="row mb-5">
+          
+
+
+          <div class="col-md-4 text-center">
+            <p class="mb-4">
+              <span class="icon-room d-block h4 text-primary"></span>
+              <span>203 Fake St. Mountain View, San Francisco, California, USA</span>
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <p class="mb-4">
+              <span class="icon-phone d-block h4 text-primary"></span>
+              <a href="#">+1 232 3235 324</a>
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <p class="mb-0">
+              <span class="icon-mail_outline d-block h4 text-primary"></span>
+              <a href="#">youremail@domain.com</a>
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 mb-5">
+
+
+            
+
+            <form action="#" class="p-5 bg-white">
+              
+              <h2 class="h4 text-black mb-5">&nbsp;</h2> 
+
+              <div class="row form-group"></div>
+
+  
+            </form>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+
+    
+<footer class="site-footer"></footer>
+
+  </div> <!-- .site-wrap -->
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+
+  
+  <script src="js/main.js"></script>
+
+  
+    
+  </body>
+</html>
